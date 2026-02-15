c339cadbd1de803c9cd1dca5693cf1289a744203
Ryusuke Kimura <37005357+RyusukeKimura@users.noreply.github.com>
2026-02-16 08:30:15 +0900

    Create mrt_revenue_performance.sql

---
+++ dbt_tutorial/models/marts/mrt_revenue_performance.sql
@@ -0,0 +1,18 @@
+with actuals as (
+    select
+        date_trunc('month', order_date) as revenue_month,
+        sum(amount) as actual_amount
+    from {{ ref('fct_orders') }}
+    group by 1
+),
+targets as (
+    select * from {{ ref('revenue_targets') }}
+)
+
+select
+    targets.month,
+    targets.target_amount,
+    coalesce(actuals.actual_amount, 0) as actual_amount,
+    (actuals.actual_amount >= targets.target_amount) as is_target_met
+from targets
+left join actuals on targets.month = actuals.revenue_month
