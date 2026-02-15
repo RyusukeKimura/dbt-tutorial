ccae85afd84b7356937f5a0bd2b789257bda794a
Ryusuke Kimura <37005357+RyusukeKimura@users.noreply.github.com>
2026-02-15 17:18:05 +0900

    Update dim_customers.sql

--- dbt_tutorial/models/dim_customers.sql
+++ dbt_tutorial/models/dim_customers.sql
@@ -6,42 +6,23 @@ with customers as (
 
 orders as (
 
-    select * from {{ ref('stg_orders') }}
-
-),
-
-payments as (
-
-    select * from {{ ref('stg_payments') }}
+    select * from {{ ref('fct_orders') }}
 
 ),
 
 customer_orders as (
 
-        select
+    select
         customer_id,
 
         min(order_date) as first_order,
         max(order_date) as most_recent_order,
-        count(order_id) as number_of_orders
+        count(order_id) as number_of_orders,
+        sum(amount) as customer_lifetime_value
+
     from orders
 
     group by customer_id
-
-),
-
-customer_payments as (
-
-    select
-        orders.customer_id,
-        sum(amount) as total_amount
-
-    from payments
-
-    left join orders on
-         payments.order_id = orders.order_id
-
-    group by orders.customer_id
 
 ),
 
@@ -54,15 +35,11 @@ final as (
         customer_orders.first_order,
         customer_orders.most_recent_order,
         customer_orders.number_of_orders,
-        customer_payments.total_amount as customer_lifetime_value
+        customer_orders.customer_lifetime_value
 
     from customers
 
-    left join customer_orders
-        on customers.customer_id = customer_orders.customer_id
-
-    left join customer_payments
-        on  customers.customer_id = customer_payments.customer_id
+    left join customer_orders using (customer_id)
 
 )
 
