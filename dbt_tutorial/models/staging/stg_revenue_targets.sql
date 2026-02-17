6fff17ca57017ed4bc04624ca6a80979d3da62f6
Ryusuke Kimura <37005357+RyusukeKimura@users.noreply.github.com>
2026-02-17 12:02:20 +0900

    Create stg_revenue_targets.sql

---
+++ dbt_tutorial/models/staging/stg_revenue_targets.sql
@@ -0,0 +1,20 @@
+with source as (
+    
+    {#-
+    Normally we would select from the table here, but we are using seeds to load
+    our data in this project
+    #}
+    select * from {{ ref('srevenue_targets') }}
+
+),
+
+renamed as (
+
+    select
+      date(month) as month,
+      cast(target_amount as int64) as target_amount,
+    from source
+
+)
+
+select * from renamed
