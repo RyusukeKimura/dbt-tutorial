f709d65ee1b2db5afea73b01d40311b52da38313
Ryusuke Kimura <37005357+RyusukeKimura@users.noreply.github.com>
2026-02-19 09:17:39 +0900

    Update assert_fct_orders_amount_equals_sum_payments.sql

--- dbt_tutorial/tests/assert_fct_orders_amount_equals_sum_payments.sql
+++ dbt_tutorial/tests/assert_fct_orders_amount_equals_sum_payments.sql
@@ -2,6 +2,11 @@
 select
     order_id,
     amount,
-    (credit_card_amount + coupon_amount + bank_transfer_amount + gift_card_amount) as calculated_total
+    (
+      credit_card_amount 
+      + coupon_amount 
+      + bank_transfer_amount 
+      + gift_card_amount
+    ) as calculated_total
 from {{ ref('fct_orders') }}
 where amount != (credit_card_amount + coupon_amount + bank_transfer_amount + gift_card_amount)
